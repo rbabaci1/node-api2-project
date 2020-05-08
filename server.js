c61edf86124ed2065d9c09b9a86eb6794d165e5b
rabah babaci <4rabah@gmail.com>
2020-05-08 09:45:07 -0700

    test the server for a simple get request

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const server = express();
 server.use(express.json());
 server.use('/api/posts', postsRouter);
 
+server.get('/', (req, res) => {
+  res.send('<h1>Bonjour Laba!!!</h1>');
+});
+
 module.exports = server;
