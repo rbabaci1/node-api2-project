d4fbb520bdd0c85e330937459e6b701fb7bcf756
rabah babaci <4rabah@gmail.com>
2020-05-08 10:08:31 -0700

    move post request handler try/catch to an else statment

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -9,17 +9,17 @@ router.post('/', async (req, res) => {
     res.status(400).json({
       errorMessage: 'Please provide title and contents for the post.',
     });
-  }
+  } else {
+    try {
+      const { id } = await insert(post);
+      const addedPost = await findById(id);
 
-  try {
-    const { id } = await insert(post);
-    const addedPost = await findById(id);
-
-    res.status(201).json(addedPost);
-  } catch (err) {
-    res.status(500).json({
-      error: 'There was an error while saving the post to the database.',
-    });
+      res.status(201).json(addedPost);
+    } catch (err) {
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database.',
+      });
+    }
   }
 });
 
