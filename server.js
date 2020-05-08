58b4cbad31653e1a3f6a82c2594a6ff961eaf997
rabah babaci <4rabah@gmail.com>
2020-05-08 09:42:37 -0700

    add the json middleware and mount the postsRouter on api/posts

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const postsRouter = require('./posts/postsRouter');
+
 const server = express();
+
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+
+module.exports = server;
